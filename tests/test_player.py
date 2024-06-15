40f42fb8002d3225e42d28e8a59bb678f5b37b10
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2024-06-15 17:10:47 +0100

    add failing tests to highlight imperfection

--- tests/test_player.py
+++ tests/test_player.py
@@ -62,3 +62,38 @@ def test_choose_next_winning_move(initial_board: list[str]):
     board.apply_turn(ai.token, chosen_tile)
     assert board.is_finished
     assert board.winner == ai
+
+
+@pytest.mark.parametrize(
+    "initial_board",
+    [
+        [
+            "x",
+            "-",
+            "-",
+            "-",
+            "o",
+            "-",
+            "o",
+            "-",
+            "x",
+        ],
+    ],
+)
+def test_block_opponent_winning_move(initial_board: list[str]):
+    # setup board 1 turn away from a win
+    board = TicTacToeBoard(initial_board)
+    ai = MaxMinAI("x")
+
+    chosen_tile = ai.tile_to_play(board)
+    board.apply_turn(ai.token, chosen_tile)
+    assert not board.is_finished
+    assert board.tiles[2] == ai.token
+
+
+def test_unbeatable():
+    """Two MaxMinAIs should always draw"""
+    board = TicTacToeBoard()
+    board.game_loop(MaxMinAI("x"), MaxMinAI("o"))
+    assert board.is_finished
+    assert board.winner is None
