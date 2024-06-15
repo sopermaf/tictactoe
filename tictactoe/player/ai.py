1120c29207b1cf2c939583106ff8e7e9d44f685f
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2024-06-15 13:41:56 +0100

    simplify MaxMinAI max_min calculation

--- tictactoe/player/ai.py
+++ tictactoe/player/ai.py
@@ -9,15 +9,14 @@ log = logging.getLogger(__name__)
 
 
 class MaxMinAI(AbstractPlayer):
-    def __init__(self, token: str) -> None:
-        super().__init__(token)
-        self._enemy = "x" if self.token == "o" else "x"  # noqa: S105
-
     def tile_to_play(self, board: TicTacToeBoard) -> int:
         """Return board tile to take turn"""
         click.echo("My Turn!")
         tile_scores = sorted(
-            ((self._rate_my_move(board, tile, 1), tile) for tile in board.free_tiles),
+            (
+                (self._rate_move(board, tile, level=1, is_my_turn=True), tile)
+                for tile in board.free_tiles
+            ),
             reverse=True,
         )
         top_scores = [t for score, t in tile_scores if score == tile_scores[0][0]]
@@ -25,18 +24,43 @@ class MaxMinAI(AbstractPlayer):
 
         return random.choice(top_scores)  # noqa: S311
 
-    def _rate_my_move(self, board: TicTacToeBoard, tile: int, level: int) -> float:
-        """Give me a score if I took this move on the given board"""
+    def _rate_move(
+        self,
+        board: TicTacToeBoard,
+        tile: int,
+        level: int,
+        *,
+        is_my_turn: bool,
+    ) -> float:
+        """
+        Uses max-min algorithm to calculate scores for each of the possible moves.
+
+        Max scores chosen when `is_my_turn` otherwise min scores chosen to simulate
+        which move the opponent would choose on their turn.
+
+        The scores are weighted by how far into the future they are, with nearer moves
+        receiving higher scores
+        """
         copy_board = TicTacToeBoard(board.tiles)
         copy_board.apply_turn(self.token, tile)
 
-        # rate the move based on their best move
         if not copy_board.is_finished:
+            move_selector_func = max if is_my_turn else min
+
             tile_scores = [
-                (self._rate_their_move(copy_board, tile, level + 1), tile)
+                (
+                    self._rate_move(
+                        board=copy_board,
+                        tile=tile,
+                        level=level + 1,
+                        is_my_turn=not is_my_turn,
+                    ),
+                    tile,
+                )
                 for tile in copy_board.free_tiles
             ]
-            return max(tile_scores)[0]
+
+            return move_selector_func(tile_scores)[0]
 
         # rate the finishing turn scores
         if copy_board.winner is None:
@@ -44,24 +68,3 @@ class MaxMinAI(AbstractPlayer):
         if copy_board.winner == self.token:
             return 1 / level
         return -1 / level
-
-    def _rate_their_move(self, board: TicTacToeBoard, tile: int, level: int) -> float:
-        """Give me a score if I took this move on the given board"""
-        # simulate the move
-        copy_board = TicTacToeBoard(board.tiles)
-        copy_board.apply_turn(self._enemy, tile)
-
-        # rate the move based on their best move
-        if not copy_board.is_finished:
-            tile_scores = [
-                (self._rate_my_move(copy_board, tile, level + 1), tile)
-                for tile in copy_board.free_tiles
-            ]
-            return min(tile_scores)[0]
-
-        # rate the finishing turn scores
-        if copy_board.winner is None:
-            return 0 / level
-        if copy_board.winner == self._enemy:
-            return -1 / level
-        return 1 / level
